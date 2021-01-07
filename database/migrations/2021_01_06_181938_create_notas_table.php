cebb4428478a1952a6f4a33f88bd37754a9291e0
BielzeraGOD <gabrielgsr2@outlook.com>
2021-01-06 22:45:09 -0300

    Prova Final

---
+++ database/migrations/2021_01_06_181938_create_notas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nota', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("aluno");
+            $table->foreign("aluno")->references("id")->on("aluno");
+            $table->integer("nota_aluno");
+			$table->string("turma",100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nota');
+    }
+}
